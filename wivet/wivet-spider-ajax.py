c0f9d28391ce123f2b22fb9018f0609fecb8e18d
psiinon <psiinon@gmail.com>
2015-12-03 13:10:32 +0000

    Added wivet script

---
+++ wivet/wivet-spider-ajax.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# This script runs the ZAP traditional and ajax spiders against wivet
+# Wivet can be installed manually or via the docker image https://hub.docker.com/r/andresriancho/wivet/
+# eg using:
+#		docker pull andresriancho/wivet
+#		docker run -i -p 443:443 andresriancho/wivet
+#
+# The following options should be used when running ZAP:
+#		ajaxSpider.clickDefaultElems=False
+# these will be set by API calls in the future (when supported).
+# Other ajaxSpider options will also affect the results - please let us know if any others improve the score :)
+#
+# If running ZAP via docker use a command like:
+# 		docker run -u zap -p 8090:8090 -d owasp/zap2docker-weekly zap-x.sh \
+#			-daemon -port 8090 -host 0.0.0.0 -config api.disablekey=true \
+#			-config ajaxSpider.clickDefaultElems=False
+# 
+# The script assumes wivet is reset after each test - otherwise you'll need to work out which result
+# you're really interested in - right now the script just picks the best result
+   
+import time, sys, getopt, re
+from pprint import pprint
+from zapv2 import ZAPv2
+   
+def main(argv):
+   # -------------------------------------------------------------------------
+   # Default Configurations - use -z/-zap and -w/-wivet for different IP addrs
+   # -------------------------------------------------------------------------
+   wivitHostIp = '172.17.0.2'
+   zapHostIp = '172.17.0.3'
+   
+   try:
+      opts, args = getopt.getopt(argv,"hz:w:",["zap=","wivet="])
+   except getopt.GetoptError:
+      print 'test.py -z <ZAPipaddr> -w <WIVITipaddr>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -z <ZAPipaddr> -w <WIVITipaddr>'
+         sys.exit()
+      elif opt in ("-z", "--zap"):
+         zapHostIp = arg
+      elif opt in ("-w", "--wivit"):
+         wivitHostIp = arg
+   print 'zap is', zapHostIp
+   print 'wivit is ', wivitHostIp
+   
+   # change this IP according to your environment
+   
+   target = 'http://' + wivitHostIp + '/'
+   
+   zap = ZAPv2(proxies={'http': 'http://' + zapHostIp + ':8090', 'https': 'http://' + zapHostIp + ':8090'})
+   
+   # Access the top page
+   zap.urlopen(target)
+   time.sleep(2)
+   
+   # Exclude pages out of scope and the logout one
+   zap.spider.exclude_from_scan(target + 'offscanpages.*')
+   zap.spider.exclude_from_scan(target + 'logout.*')
+
+   zap.spider.scan(target)
+   print 'Spider started'
+   # Give the Spider a chance to start
+   time.sleep(5)
+   while (int(zap.spider.status()) < 100):
+       print 'Spider progress %: ' + zap.spider.status()
+       time.sleep(5)
+
+   print 'Spider completed'
+
+   time.sleep(5)
+
+   # Just interested in the time the ajax spider takes
+   start = time.time()
+
+   zap.ajaxSpider.scan(target)
+   print 'Ajax Spider started'
+   # Give the Ajax Spider a chance to start
+   time.sleep(5)
+   while (zap.ajaxSpider.status == 'running'):
+       print 'Ajax spider still running, results: ' + zap.ajaxSpider.number_of_results
+       time.sleep(5)
+
+   print 'Ajax Spider completed'
+
+   end = time.time()
+   print 'Time: ' + time.strftime('%H:%M:%S', time.gmtime(end - start))
+   print 'Pages: ' + zap.ajaxSpider.number_of_results
+
+   # parse and extract score
+   stats = zap.urlopen(target + 'offscanpages/statistics.php')
+   scores = re.findall('Coverage: \%(.+?)\)', stats)
+   top_score = 0
+   for score in scores:
+      # This assumes wivet is reset after each test
+      if int(score) > top_score:
+         top_score = int(score)
+   print 'Score: ' + str(top_score)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])   
