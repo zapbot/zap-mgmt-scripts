2082f58497597c367bd7d94de0861d7635cfadc5
psiinon <psiinon@gmail.com>
2015-12-16 16:19:42 +0000

    Parameterized the wavsep scripts

--- wavsep/wavsep-1.5-spider-scan.py
+++ wavsep/wavsep-1.5-spider-scan.py
@@ -10,20 +10,26 @@ def main(argv):
    # -------------------------------------------------------------------------
    wavsepHostIp = '172.17.0.2'
    zapHostIp = '172.17.0.3'
+   ajax = False
+   policy = None
    
    try:
-      opts, args = getopt.getopt(argv,"hz:w:",["zap=","wavsep="])
+      opts, args = getopt.getopt(argv,"haztp",["zap=","wavsep=","ajax","policy="])
    except getopt.GetoptError:
       print 'test.py -z <ZAPipaddr> -w <WAVSEPipaddr>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'test.py -z <ZAPipaddr> -w <WAVSEPipaddr>'
+         print 'test.py -z <ZAPipaddr> -w <WAVSEPipaddr> -p <scanPolicy> -a'
          sys.exit()
       elif opt in ("-z", "--zap"):
          zapHostIp = arg
       elif opt in ("-w", "--wavsep"):
          wavsepHostIp = arg
+      elif opt in ("-a", "--ajax"):
+         ajax = True
+      elif opt in ("-p", "--policy"):
+         ploicy = arg
    print 'zap is', zapHostIp
    print 'wavsep is ', wavsepHostIp
    
@@ -66,7 +72,7 @@ def main(argv):
    zap.urlopen(activeIndexPage)
    time.sleep(2)
 
-   print 'Spider started'
+   print 'Spidering %s' % activeIndexPage
    zap.spider.scan(activeIndexPage)
    # Give the Spider a chance to start
    time.sleep(5)
@@ -75,11 +81,29 @@ def main(argv):
        time.sleep(5)
 
    print 'Spider completed'
-
    time.sleep(5)
+   
+   if (ajax):
+       # Run the Ajax Spider
+       print 'Ajax Spidering %s' % activeIndexPage
+       zap.ajaxSpider.scan(activeIndexPage)
+       # Give the Ajax Spider a chance to start
+       time.sleep(5)
+       while (zap.ajaxSpider.status == 'running'):
+           print 'Ajax spider still running, results: ' + zap.ajaxSpider.number_of_results
+           time.sleep(5)
+       print 'Ajax Spider completed'
+       time.sleep(5)
+
+   # Create the policy
 
    print 'Scanning target %s' % target
-   zap.ascan.scan(target)
+   if (policy):
+       zap.ascan.scan(target, scanpolicyname=policy)
+   else:
+       zap.ascan.scan(target)
+   # Give the Scanner a chance to start
+   time.sleep(5)
    while (int(zap.ascan.status()) < 100):
        print 'Scan progress %: ' + zap.ascan.status()
        time.sleep(5)
---
+++ wavsep/zap-weekly-vs-wavsep-1.5.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+# Runs ZAP against wavsep with the specified options, generates the report and issues a PR to update it
+#
+# Some example calls:
+# ./zap-vs-wavsep-1.5.sh -d "owasp/zap2docker-weekly" -e "Score 66" -s "wavsep-1.5-weekly-RB-M-M" -t "Rel,Beta"
+# ./zap-vs-wavsep-1.5.sh -d "owasp/zap2docker-weekly" -e "Score 66" -p "St-High-Th-Med" -s "wavsep-1.5-weekly-RB-H-M" -t "Rel,Beta"
+# ./zap-vs-wavsep-1.5.sh -d "owasp/zap2docker-weekly" -e "Score 66" -s "wavsep-1.5-weekly-RBA-M-M" -t "Rel,Beta" -z "-addoninstall ascanrulesAlpha"
+
+USAGE="Usage: $0 -d docker-image -e expected-score -n name -t text -z zap-options"
+
+# Handle the options
+docker=''
+expected=''
+name=''
+policy='Default policy'
+text=''
+zap_opt=''
+score_opt=''
+
+while getopts "ad:e:n:p:z:" optname
+  do
+    case "$optname" in
+      "a")
+        # Ajax Spider
+        score_opt="-a"
+        ;;
+      "d")
+        # Docker image (mandatory)
+        docker=$OPTARG
+        ;;
+      "e")
+        # The expected score string
+        expected=$OPTARG
+        ;;
+      "n")
+        # The name to use in the results files
+        name=$OPTARG
+        ;;
+      "p")
+        # Scan Policy (mandatory)
+        policy=$OPTARG
+        ;;
+      "t")
+        text=$OPTARG
+        ;;
+      "z")
+        # ZAP command line options
+        zap_opt=$OPTARG
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+  done
+# Check for the mandatory ones
+if [ "$docker" == "" ]
+then
+  echo "Missing docker"
+  echo $USAGE
+  exit 2
+fi
+if [ "$name" == "" ]
+then
+  echo "Missing name"
+  echo $USAGE
+  exit 2
+fi
+if [ "$policy" == "" ]
+then
+  echo "Missing policy"
+  echo $USAGE
+  exit 2
+fi
+
+# Git stuff
+git config --global user.name "zapbot"
+git config --global user.email "zapbot@zaproxy.org"
+
+# The wrk directory will probably exist, but just in case..
+mkdir ~/wrk
+# Start a new out.txt file with the date
+date > ~/wrk/out.txt
+
+# Download and start new docker instances
+docker pull vianma/wavsep:v15
+WSCID=$(sudo docker run -d -p 8080:8080 vianma/wavsep:v15)
+echo "WSCID=$WSCID" >> ~/wrk/out.txt
+
+IP=$(docker inspect $WSCID | grep IPAddress | tail -1)
+echo "IP=$IP" >> ~/wrk/out.txt
+IP=$(echo $IP | awk '{print substr($0,15)}')
+echo "IP=$IP" >> ~/wrk/out.txt
+
+WSIP=$(echo ${IP::-2})
+echo "WSIP=$WSIP" >> ~/wrk/out.txt
+echo Wavsep IP = $WSIP
+
+docker pull owasp/zap2docker-weekly
+ZPCID=$(sudo docker run -u zap -p 8090:8090 -d $docker zap-x.sh -daemon -port 8090 -host 0.0.0.0 -config api.disablekey=true $zap_opt)
+
+IP=$(docker inspect $ZPCID | grep IPAddress | tail -1)
+IP=$(echo $IP | awk '{print substr($0,15)}')
+
+ZPIP=$(echo ${IP::-2})
+echo "====" >> ~/wrk/out.txt
+echo "ZPCID=$ZPCID" >> ~/wrk/out.txt
+echo "ZPIP=$ZPIP" >> ~/wrk/out.txt
+echo ZAP IP = $ZPIP
+
+echo Let ZAP start up...
+sleep 20
+
+# Spider and scan the app
+python ~/zap-mgmt-scripts/wavsep/wavsep-1.5-spider-scan.py $score_opt -p $policy -z $ZPIP -w $WSIP >> ~/wrk/out.txt
+
+# Save the logs
+LOG="~/zap-mgmt-scripts_gh-pages/reports/${name}.logs.txt"
+docker logs $ZPCID > ~/zap-mgmt-scripts_gh-pages/reports/${name}.logs.txt
+
+
+# Generate the report
+python ~/zap-mgmt-scripts/wavsep/wavsep-score.py -h $ZPIP > ~/wrk/summary.txt
+echo "====" >> ~/wrk/out.txt
+echo "Summary" >> ~/wrk/out.txt
+cat ~/wrk/summary.txt >> ~/wrk/out.txt
+
+echo "<TR>" > ${name}.summary
+echo "<td>" `cat ~/wrk/summary.txt | grep ZAP | awk -F ' ' '{print $2}'` "</td>" >> ${name}.summary
+
+echo "<td>wavsep 1.5</td>" >> ${name}.summary
+echo "<td>$text</td>" >> ${name}.summary
+echo "<td>$policy</td>" >> ${name}.summary
+
+echo "<td>" `date --rfc-3339 date` "</td>" >> ${name}.summary
+SC=$(cat ~/wrk/summary.txt | grep Score | awk -F ' ' '{print $2}')
+echo "<td><a href=\"reports/${name}.html\">${SC} &#37;</a></td>" >> ${name}.summary
+echo "<td>" `cat ~/wrk/summary.txt | grep urls | awk -F ' ' '{print $2}'` "</td>" >> ${name}.summary
+echo "<td>" `cat ~/wrk/summary.txt | grep Took | awk -F ' ' '{print $2}'` "</td>" >> ${name}.summary
+
+ERRS=$(grep -c ERROR ~/zap-mgmt-scripts_gh-pages/reports/${name}.logs.txt) >> ${name}.summary
+echo "<td><a href=\"reports/${name}.logs.txt\">${ERRS}</a></td>" >> ${name}.summary
+echo "</tr>" >> ${name}.summary
+
+cat ${name}.summary
+
+# If present ~/.awssns should contain the AWS SNS topic to post to
+AWSSNSF=$(echo ~/.awssns)
+
+if [ "$SC" == "" ]; then
+  # Something went wrong score, send an alert
+  AWSSNS=$(cat ~/.awssns)
+  OUTFILE=$(cat ~/wrk/out.txt)
+  aws sns publish --subject "ZAP ${name} Failed to run properly" --message "$OUTFILE" --topic $AWSSNS
+  exit
+elif [ "$SC" != "$expected" ]; then
+  # Unexpected score, send an alert
+  AWSSNS=$(cat ~/.awssns)
+  aws sns publish --subject "ZAP ${name} Expected '$expected' got '$SC'" --message "$OUTFILE" --topic $AWSSNS
+fi
+
+# Save and push to github
+mv ${name}.summary ~/zap-mgmt-scripts_gh-pages/reports/
+mv report.html ~/zap-mgmt-scripts_gh-pages/reports/${name}.html
+
+cd ~/zap-mgmt-scripts_gh-pages
+git add reports
+
+cat scan.head reports/*.summary scan.tail > scans.html
+git add scans.html
+
+git commit -m "Report ${name}"
+git push origin
+
