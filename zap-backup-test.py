5a62764d28d19c3dfa58753447861c6bb03b8a0e
zapbot <zapbot@zaproxy.org>
2016-01-21 11:52:02 +0000

    Check alert counts too

--- zap-backup-test.py
+++ zap-backup-test.py
@@ -28,8 +28,10 @@ def main(argv):
    zap = ZAPv2(proxies={'http': zapHostPort, 'https': zapHostPort})
    
    # Count number of messages
-   old_count = zap.core.number_of_messages()
-   print 'Initial msg count: %s' % old_count
+   old_mcount = zap.core.number_of_messages()
+   old_acount = zap.core.number_of_alerts()
+   print 'Initial msg count: %s' % old_mcount
+   print 'Initial alert count: %s' % old_acount
 
    # Time backup
    start_time = time.time()
@@ -54,11 +56,16 @@ def main(argv):
    rec_time = (time.time() - start_time)
    print 'Loaded: %s' % str(time.strftime('%H:%M:%S', time.gmtime(int(rec_time))))
 
-   rec_count = zap.core.number_of_messages()
-   if (old_count == rec_count):
-     print 'PASS: counts match'
+   rec_mcount = zap.core.number_of_messages()
+   rec_acount = zap.core.number_of_alerts()
+   if (old_mcount == rec_mcount):
+     print 'PASS: msg counts match'
    else:
-     print 'FAIL: Counts differ - original: %s recovered: %s' % old_count, new_count
+     print 'FAIL: msg counts differ - original: %s recovered: %s' % old_mcount, new_mcount
+   if (old_acount == rec_acount):
+     print 'PASS: alert counts match'
+   else:
+     print 'FAIL: alert counts differ - original: %s recovered: %s' % old_acount, new_acount
 
 
 if __name__ == "__main__":
