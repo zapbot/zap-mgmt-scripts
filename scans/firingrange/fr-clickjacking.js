2f88009ad571a319fb1b66bfc5ad4cb2b4b5fef4
Simon Bennetts <psiinon@gmail.com>
2024-05-20 12:48:57 +0100

    Firing Range - fix config files
    Should have been done in
    https://github.com/zapbot/zap-mgmt-scripts/pull/190 :)
    
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- scans/firingrange/fr-clickjacking.js
+++ scans/firingrange/fr-clickjacking.js
@@ -1,6 +1,6 @@
 var DIR = "/zap/wrk/";
-var NAME = "Invalid framing configuration";
-var TARGET = "invalidframingconfig";
+var NAME = "Clickjacking";
+var TARGET = "clickjacking";
 var RULES = [10020,10055];
 var MIN_LEVEL = 1;
 var IGNORE_PATHS = ['/'];
