df680775e2f86d3396312c3b97d0d4aaf4a25d6f
kingthorin <kingthorin@users.noreply.github.com>
2025-02-24 14:12:46 -0500

    auth tests: get the site from the report for output
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- scans/auth/auth_plan_tests.sh
+++ scans/auth/auth_plan_tests.sh
@@ -7,8 +7,8 @@ runplan()
     FILE=$2
     TYPE=$3
     echo "Target: $TARGET Plan: $FILE"
-    echo -ne "$INDENT"- type: "$TYPE\n"|tee -a "$OUTPUT" > /dev/null
-    echo -ne "$INDENT$INDENT"auth:|tee -a "$OUTPUT" > /dev/null
+    echo -ne "$INDENT$INDENT$TYPE:\n"|tee -a "$OUTPUT" > /dev/null
+    echo -ne "$INDENT$INDENT$INDENT"auth:|tee -a "$OUTPUT" > /dev/null
 
     /zap/zap.sh -cmd -autorun "$FILE"
     RET=$?
@@ -61,13 +61,13 @@ getreportdetails()
 {
     AUTHREPORT=$1
     USER_SUCCESS=`jq -r '.summaryItems[] | select(.key == "auth.summary.username") | .passed' $AUTHREPORT`
-    echo "$INDENT$INDENT"username: $USER_SUCCESS|tee -a "$OUTPUT" > /dev/null
+    echo "$INDENT$INDENT$INDENT"username: $USER_SUCCESS|tee -a "$OUTPUT" > /dev/null
     PASS_SUCCESS=`jq -r '.summaryItems[] | select(.key == "auth.summary.password") | .passed' $AUTHREPORT`
-    echo "$INDENT$INDENT"password: $PASS_SUCCESS|tee -a "$OUTPUT" > /dev/null
+    echo "$INDENT$INDENT$INDENT"password: $PASS_SUCCESS|tee -a "$OUTPUT" > /dev/null
     SESS_SUCCESS=`jq -r '.summaryItems[] | select(.key == "auth.summary.session") | .passed' $AUTHREPORT`
-    echo "$INDENT$INDENT"session: $SESS_SUCCESS|tee -a "$OUTPUT" > /dev/null
+    echo "$INDENT$INDENT$INDENT"session: $SESS_SUCCESS|tee -a "$OUTPUT" > /dev/null
     VERIF_SUCCESS=`jq -r '.summaryItems[] | select(.key == "auth.summary.verif") | .passed' $AUTHREPORT`
-    echo "$INDENT$INDENT"verification: $VERIF_SUCCESS|tee -a "$OUTPUT" > /dev/null
+    echo "$INDENT$INDENT$INDENT"verification: $VERIF_SUCCESS|tee -a "$OUTPUT" > /dev/null
 }
 
 RES=0
@@ -104,12 +104,24 @@ do
 
             export username=$(eval echo \$\{TARGET\}_user)
             export zapusername=${!username}
-            
+
+            echo -ne "$INDENT"|tee -a "$OUTPUT" > /dev/null
+            echo -ne "site: \"$zapsite\"\n"|tee -a "$OUTPUT" > /dev/null
+
+            if [ -f notes.txt ]
+            then
+                echo -ne "$INDENT"|tee -a "$OUTPUT" > /dev/null
+                echo -ne "note: "|tee -a "$OUTPUT" > /dev/null
+                echo "\"$(cat notes.txt)\""|tee -a "$OUTPUT" > /dev/null
+            fi
+
+            echo -ne "$INDENT"|tee -a "$OUTPUT" > /dev/null
+            echo -ne "plans:\n"|tee -a "$OUTPUT" > /dev/null
             runplan $TARGET /zap/wrk/scans/auth/bba-auth-test.yaml "stdbba"
         else
             echo "No $TARGET/config file"
         fi
-        
+
         shopt -s nullglob  # May be no yaml files
         for file in *.yaml
         do
@@ -118,12 +130,6 @@ do
         done
         shopt -u nullglob
 
-        if [ -f notes.txt ]
-        then
-            echo -ne "$INDENT"|tee -a "$OUTPUT" > /dev/null
-            echo -ne "- note: "|tee -a "$OUTPUT" > /dev/null
-            echo "\"$(cat notes.txt)\""|tee -a "$OUTPUT" > /dev/null
-        fi
         cd ..
     fi
 done
