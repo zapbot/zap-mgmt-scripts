f892b5ebc90457c59506ec0b33dd5bc26a2a8bdb
Simon Bennetts <psiinon@gmail.com>
2026-04-30 11:48:26 +0200

    FR score address and angular sections
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

---
+++ scans/firingrange/fr-address.js
@@ -0,0 +1,7 @@
+var DIR = "/zap/wrk/";
+var NAME = "Address DOM XSS";
+var TARGET = "address";
+var RULES = [40012, 40026, 200002, 200007, 210000, 220000, 220003];
+var MIN_LEVEL = 2;
+var IGNORE_PATHS = [];
+var BROKEN_PATHS = [];
---
+++ scans/firingrange/fr-angular.js
@@ -0,0 +1,7 @@
+var DIR = "/zap/wrk/";
+var NAME = " Angular-based XSSes DOM XSS";
+var TARGET = "angular";
+var RULES = [40012, 200002, 210000, 220000, 220002];
+var MIN_LEVEL = 2;
+var IGNORE_PATHS = [];
+var BROKEN_PATHS = [];
