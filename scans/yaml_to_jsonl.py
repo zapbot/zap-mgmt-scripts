09020f62a175c46939d5c37cda309ad68ca4d60b
Simon Bennetts <psiinon@gmail.com>
2026-07-07 16:11:09 +0100

    Firing range - push to S3 (#289)
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

---
+++ scans/yaml_to_jsonl.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Convert ZAP scan result YAML files to JSON Lines for S3/Athena storage.
+
+Usage: yaml_to_jsonl.py <date> <scan> <yaml_file> [<yaml_file> ...]
+Output: JSON Lines to stdout, one record per tested path.
+"""
+import json
+import sys
+import yaml
+
+
+def convert(date, scan, yaml_path):
+    with open(yaml_path) as f:
+        data = yaml.safe_load(f)
+    section = data['section']
+    for detail in data.get('details', []):
+        record = {
+            'date': date,
+            'scan': scan,
+            'section': section,
+            'path': detail['path'],
+            'result': detail['result'],
+            'rules': detail.get('rules', []),
+        }
+        print(json.dumps(record))
+
+
+if __name__ == '__main__':
+    date = sys.argv[1]
+    scan = sys.argv[2]
+    for yaml_path in sys.argv[3:]:
+        convert(date, scan, yaml_path)
